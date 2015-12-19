169795e591721fa2b6f0fc8def06b765dd4e38db
ear <41918235@qq.com>
2015-12-19 20:43:19 +0800

    多行定义

--- system.f
+++ system.f
@@ -1,2 +1,13 @@
-:x if 22 22 else 33 33 endif ;
-:s switch 1 case 11 break 2 case 22 break 3 case 33 break ends ;
+:x	if 22 22 else 33 33 endif ;
+:true	1 ;
+:false	0 ;
+:u<	swap u> ;
+:<	swap > ;
+:default sameAs r@ ;
+:s
+switch 
+	1 case 11 break 
+	2 case 22 break
+	3 case 33 break 
+	default case 55 break
+ends ;
