a37beba21f5d7276e1440090d5e2d26d096d4866
ear <41918235@qq.com>
2015-12-20 12:38:09 +0800

    修改switch

--- system.f
+++ system.f
@@ -1,13 +1,14 @@
-:x	if 22 22 else 33 33 endif ;
-:true	1 ;
-:false	0 ;
-:u<	swap u> ;
-:<	swap > ;
-:default sameAs r@ ;
-:s
+: true	1 ;
+: false	0 ;
+: u<	swap u> ;
+: <	swap > ;
+: default drop ;
+: do	loop ;
+: x	if 22 22 else 33 33 endif ;
+: s
 switch 
 	1 case 11 break 
 	2 case 22 break
 	3 case 33 break 
-	default case 55 break
+	default 55 
 ends ;
